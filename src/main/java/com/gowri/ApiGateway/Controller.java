413bc660b4c109f4b59aa099f8c6fb321ae85ade
gowrish9595@gmail.com <gowrish9595@gmail.com>
2022-10-09 17:24:08 +0800

    New: Login Controller

--- src/main/java/com/gowri/ApiGateway/Controller.java
+++ src/main/java/com/gowri/ApiGateway/Controller.java
@@ -1,7 +1,9 @@
 package com.gowri.ApiGateway;
 
 import com.gowri.ApiGateway.domain.IncomingRequest;
+import com.gowri.ApiGateway.handler.impl.AuthenticationHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @RestController
@@ -18,7 +22,11 @@ public class Controller {
     @Autowired
     private RoutingGatewayClient routingGatewayClient;
 
-    @RequestMapping("/**")
+
+    @Autowired
+    AuthenticationHandler authenticationHandler;
+
+    @RequestMapping("/v1/**")
     public ResponseEntity<Object> get(HttpServletRequest request, HttpServletResponse resp) throws IOException {
         String method = request.getMethod();
         String uri = request.getRequestURI();
@@ -27,6 +35,16 @@ public class Controller {
         request1.setUrl(uri);
         request1.setHttpMethod(HttpMethod.resolve(method));
         request1.setBody(body);
+        HttpHeaders httpHeaders = Collections.list(request.getHeaderNames())
+                .stream()
+                .collect(Collectors.toMap(
+                        Function.identity(),
+                        h -> Collections.list(request.getHeaders(h)),
+                        (oldValue, newValue) -> newValue,
+                        HttpHeaders::new
+                ));
+        request1.setHttpHeaders(httpHeaders);
+        authenticationHandler.handle(request1);
         return routingGatewayClient.send(request1);
     }
 }
---
+++ src/main/java/com/gowri/ApiGateway/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.gowri.ApiGateway.controller;
+
+import com.gowri.ApiGateway.util.JwtTokenUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @Autowired
+    private JwtTokenUtil keyUtil;
+
+    @GetMapping("/login")
+    public String login() {
+        return keyUtil.generateToken().orElseThrow(IllegalArgumentException::new);
+    }
+}
